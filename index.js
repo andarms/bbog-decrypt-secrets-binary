3c181f0cbcf2b334e6855a06318417d295735eb9
Adrian Manjarres <amanja4@bancodebogota.com.co>
2021-05-12 08:03:49 -0500

    initial lambda setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const execFile = require("child_process").execFile;
+exports.handler = async function (event, context) {
+  const data = await getSecrets();
+  return data;
+};
+
+function getSecrets() {
+  const params = [
+    "--region",
+    "sa-east-1",
+    "--cmk",
+    process.env.AWS_CMK_ARN_ID,
+    "--json",
+    JSON.stringify({ ARMS: process.env.ARMS }),
+  ];
+  let promise = new Promise((resolve, reject) => {
+    exec("aws-decrypt", params, null, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+  return promise;
+}
