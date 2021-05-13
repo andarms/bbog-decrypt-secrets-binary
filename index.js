e7df5e59371174f1103ad589568676b82b43cf49
Adrian Manjarres <amanja4@bancodebogota.com.co>
2021-05-13 07:14:27 -0500

    Change js execution of binary

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const execFile = require("child_process").execFile;
+const { exec } = require("child_process");
 const path = require("path");
 
 exports.handler = async function (event, context) {
-  const data = await getSecrets();
+  const data = getSecrets();
   return data;
 };
 
 function getSecrets() {
-  const params = [
-    "--region",
-    "sa-east-1",
-    "--cmk",
-    process.env.AWS_CMK_ARN,
-    "--json",
-    JSON.stringify({ ARMS: process.env.ARMS }),
-  ];
-  let promise = new Promise((resolve, reject) => {
-    execFile("bbog-dig-secret-decrypt", params, null, (err, data) => {
-      if (err) reject(err);
-      else resolve(data);
-    });
+  const command = `bbog-dig-secret-decrypt --region sa-east-1 --cmk ${process.env.AWS_CMK_ARN} --json "{ ARMS: ${process.env.ARMS}" }`;
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    return stdout;
   });
-  return promise;
 }
