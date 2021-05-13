4fc9ba11cfb3823c78d02fc8edc43deaa1f17b25
Adrian Manjarres <amanja4@bancodebogota.com.co>
2021-05-13 07:29:36 -0500

    Add asyc to bin execution

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const { exec } = require("child_process");
 const path = require("path");
 
 exports.handler = async function (event, context) {
-  const data = getSecrets();
+  const data = await getSecrets();
   return data;
 };
 
 function getSecrets() {
   const command = `bbog-dig-secret-decrypt --region sa-east-1 --cmk ${process.env.AWS_CMK_ARN} --json "{ ARMS: ${process.env.ARMS}" }`;
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      return;
-    }
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      }
 
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
 
-    return stdout;
+      resolve(stdout);
+    });
   });
 }
